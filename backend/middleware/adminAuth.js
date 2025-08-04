b2a9b9891e4e44e56702f6ed15036ddc716ff09d
CodeWithUtkarsha <usalve@gitam.in>
2025-08-04 19:06:27 +0530

    redesigned

--- backend/middleware/adminAuth.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res, next) => {
-  // Check if user is admin
-  if (req.user.role !== "admin") {
-    return res.status(403).json({ message: "Access denied. Admin privileges required." })
-  }
-
-  next()
-}
-
